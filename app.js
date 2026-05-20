718641ec5f7a841c75b4c2e36ddc0b5848e164f6
Mohammad Azaan <azanqureshi339@gmail.com>
2026-05-20 16:22:40 +0500

    files initialized

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("aj meri javascript ki first class hai")
+
+var schoolName ="Sir Syed Sch0ol"
+var class = "1
+var age ="15"
---
+++ index.js
@@ -0,0 +1,20 @@
+// var name="Azaan"
+// var className="14"
+// var age="15"
+// var address="..........."
+// console.log(name)
+// console.log(age)
+// console.log(address)
+// console.log()
+
+
+// console.log( typeof "10")//string
+// console.log(  typeof 3455566677777)//number
+// console.log(   typeof true)//boolean
+// console.log(  typeof false)//boolean
+
+ var name ="azaan";
+ var age =15
+ var schoolJaega=false;
+ 
+
